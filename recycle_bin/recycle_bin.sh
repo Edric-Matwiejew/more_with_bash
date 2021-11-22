f7b34ade0987dd806b3db9e1a6faee921ddc07d6
Edric Matwiejew <43461963+Edric-Matwiejew@users.noreply.github.com>
2021-11-23 02:55:40 +0800

    Update recycle_bin.sh

--- recycle_bin/recycle_bin.sh
+++ recycle_bin/recycle_bin.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd $(dirname ${BASH_SOURCE[0]});pwd)"
-
+SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}");pwd)"
 RECYCLE_BIN_DIR="$SCRIPT_DIR/.recycle_bin"
+LOG_PATH="$RECYCLE_BIN_DIR"/.recycle_log
 
 if [ ! -e "$RECYCLE_BIN_DIR" ]
 then
-	mkdir $RECYCLE_BIN_DIR
-	echo "Created new recycle bin directory at :" $RECYCLE_BIN_DIR
+	mkdir "$RECYCLE_BIN_DIR"
+	echo "Created new recycle bin directory at :" "$RECYCLE_BIN_DIR"
 fi
 
 del () {
 
 	# get the number after an option flag.
-
 	get_number () {
 	
 		N_OPTION=$(echo $1| sed 's/[^0-9]//g')
@@ -27,16 +26,12 @@ del () {
 		fi
 		}
 
-	LOG_PATH="$RECYCLE_BIN_DIR"/.recycle_log
-
-	# extract the number attached to an OPTION flag.
-
 	if [ ! -e "$LOG_PATH" ]
 	then
 		touch "$LOG_PATH"
 	fi
 
-	if $($(echo $@ | grep -q '\-h') || $(echo $@ | grep -q '\--help'))
+	if [ $(echo $@ | grep -q '\-h') || $(echo $@ | grep -q '\--help') ]
 	then
 		echo "Usage: del [OPTION]... [FILE]..."
 		echo "Move FILE(s) to a recycle bin folder."
@@ -54,13 +49,13 @@ del () {
 	FILES=()
 	OPTIONs=()
 
-	for input in $@
+	for input in "$@"
 	do
-		if $(echo $input | grep -Eq '\-[a-zA-Z?][0-9*]')
+		if echo $INPUT | grep -Eq '\-[a-zA-Z?][0-9*]'
 		then
-			OPTIONs+=($input)
+			OPTIONs+=("$INPUT")
 		else
-			FILES+=($input)
+			FILES+=("$INPUT")
 		fi
 	done
 
@@ -78,23 +73,23 @@ del () {
 		case $OPTION in
 
 			-l*)
-				tail -n$(get_number $OPTION) $LOG_PATH
+				tail -n$(get_number "$OPTION") "$LOG_PATH"
 				return 0
 				;;
 
 			-u*)
-				N=$(get_number $OPTION)
-				mv_from=($(tail -n$N $LOG_PATH | awk '{print $2}'))
-				mv_to=($(tail -n$N $LOG_PATH | awk '{print $3}'))
+				N=$(get_number "$OPTION")
+				mv_from=($(tail -n$N "$LOG_PATH" | awk '{print $2}'))
+				mv_to=($(tail -n$N "$LOG_PATH" | awk '{print $3}'))
 
 				for i in $(seq 0 $((N - 1)))
 				do
-					mv ${mv_from[$i]} ${mv_to[$i]}
+					mv "${mv_from[$i]}" "${mv_to[$i]}"
 
-					if [ ! -e ${mv_from[$i]} ]
+					if [ ! -e "${mv_from[$i]}" ]
 					then
-						M=$(grep -n "${mv_from[$i]}" $LOG_PATH | cut -f1 -d:)
-						sed -i -e "${M}d" $LOG_PATH
+						M=$(grep -n "${mv_from[$i]}" "$LOG_PATH" | cut -f1 -d:)
+						sed -i -e "${M}d" "$LOG_PATH"
 					fi
 
 				done
@@ -109,15 +104,14 @@ del () {
 	fi
 
 	#if no OPTION is passed, delete FILES
-
 	for FILE in ${FILES[@]}
 	do
-		if [ -e $FILE ]
+		if [ -e "$FILE" ]
 		then
 			NEW_PATH="$RECYCLE_BIN_DIR/$(date +%s)_$FILE"
 			OLD_PATH="$(cd $(dirname ${BASH_SOURCE[0]});pwd)/$(basename "$FILE")"
 			echo $(du "$OLD_PATH" | cut -f1)  "$NEW_PATH" "$OLD_PATH" >> "$LOG_PATH"
-			mv $FILE $NEW_PATH
+			mv "$FILE" "$NEW_PATH"
 		else
 			echo "File '$FILE' does not exist!"
 		fi
